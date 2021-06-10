b03d25e27a864b22c72b0e7c4c7326d4295ba4bd
yotammarton <yonam92@gmail.com>
2021-06-10 11:32:37 +0300

    change download

--- download_data.py
+++ download_data.py
@@ -5,10 +5,10 @@ import tarfile
 output = '/home/student/train.tar'
 # gdown.download(url, output, quiet=False)
 tf = tarfile.open(output)
-tf.extractall()
+tf.extractall('/home/student/')
 
 # url = 'https://drive.google.com/uc?id=1JVCAqZOhKCs3_5KrlZakB-ZJZTcyZSS3'
 output = '/home/student/test.tar'
 # gdown.download(url, output, quiet=False)
 tf = tarfile.open(output)
-tf.extractall()
+tf.extractall('/home/student/')
