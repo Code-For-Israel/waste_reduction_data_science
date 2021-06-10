535e131a51ac53ce57063a453b7f9c5c8aaf64b9
yotammarton <yonam92@gmail.com>
2021-06-10 11:29:46 +0300

    change download

--- download_data.py
+++ download_data.py
@@ -1,7 +1,6 @@
 import gdown
 import tarfile
 
-
 # url = 'https://drive.google.com/uc?id=1GsC6vtBm47kNUPrCU-8_hXU1Pi0BraE9'
 output = '/home/student/train.tar'
 # gdown.download(url, output, quiet=False)
