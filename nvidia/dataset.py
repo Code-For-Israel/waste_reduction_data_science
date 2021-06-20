97edc3501bb688e545108974967a4e66885caf10
yotammarton <yonam92@gmail.com>
2021-06-20 19:32:32 +0300

    try NVIDIA SSD30 model from https://github.com/NVIDIA/DeepLearningExamples/blob/master/PyTorch/Detection/SSD

---
+++ nvidia/dataset.py
@@ -0,0 +1,109 @@
+import torch
+from torch.utils.data import Dataset
+import json
+import os
+from PIL import Image
+from utils import transform
+import constants
+import concurrent.futures
+
+
+class MasksDataset(Dataset):
+    """
+    call example: MasksDataset(data_folder=constants.TRAIN_IMG_PATH, split='train')
+    """
+
+    def __init__(self, data_folder, split):
+        self.split = split.upper()
+        assert self.split in {'TRAIN', 'TEST'}
+
+        self.data_folder = data_folder
+
+        # Read data file names
+        self.images = os.listdir(data_folder)
+        if self.split == 'TRAIN':
+            # exclude problematic images with width or heigh equal to 0
+            paths_to_exclude = []
+            for path in self.images:
+                image_id, bbox, proper_mask = path.strip(".jpg").split("__")
+                x_min, y_min, w, h = json.loads(bbox)  # convert string bbox to list of integers
+                if w <= 0 or h <= 0:
+                    paths_to_exclude.append(path)
+            self.images = [path for path in self.images if path not in paths_to_exclude]
+
+        # Load data to RAM using multiprocess
+        self.loaded_imgs = []
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.load_single_img, path) for path in self.images]
+            self.loaded_imgs = [fut.result() for fut in futures]
+        self.loaded_imgs = sorted(self.loaded_imgs, key=lambda x: x[0])  # sort the images to reproduce results
+        print(f"Finished loading {self.split} set to memory - total of {len(self.loaded_imgs)} images")
+
+        # Store images sizes
+        self.sizes = []
+        for path in self.images:
+            image = Image.open(os.path.join(self.data_folder, path), mode='r').convert('RGB')
+            self.sizes.append(torch.FloatTensor([image.width, image.height, image.width, image.height]).unsqueeze(0))
+
+    def __getitem__(self, i):
+        sample = self.loaded_imgs[i]
+        # TODO YOTAM if split == TRAIN make transformations (crop, flip ...)
+
+        if self.split == 'TRAIN':
+            img = sample[1]
+            # TODO Augmentation on img - make a copy first (!!!!!!)
+            return img, sample[2], sample[3]
+        else:  # TEST
+            return sample[1], sample[2], sample[3]  # image, box, label
+
+    def __len__(self):
+        return len(self.images)
+
+    def load_single_img(self, path):
+        image_id, bbox, proper_mask = path.strip(".jpg").split("__")
+        x_min, y_min, w, h = json.loads(bbox)  # convert string bbox to list of integers
+        bbox = [x_min, y_min, x_min + w, y_min + h]  # [x_min, y_min, x_max, y_max]
+        proper_mask = [1] if proper_mask.lower() == "true" else [2]
+
+        # Read image
+        image = Image.open(os.path.join(self.data_folder, path), mode='r').convert('RGB')
+        image = image.convert('RGB')
+
+        box = torch.FloatTensor(bbox)  # (1, 4)
+        label = torch.LongTensor(proper_mask)  # (1)
+
+        # Apply transformations
+        image, box, label = transform(image, box, label, split=self.split)
+
+        return image_id, image, box, label
+
+
+if __name__ == '__main__':
+    # check MasksDataset class
+    # total of ~22GB RAM are needed
+    # train
+    dataset = MasksDataset(data_folder=constants.TRAIN_IMG_PATH, split='train')
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=20, shuffle=True)
+    # (images, boxes, labels) = next(iter(train_loader))
+
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for i, (images, boxes, labels) in enumerate(train_loader):
+        data = images
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
+
+    print('train pixel mean values', mean)
+    print('train pixel std values', std)
+
+    # test
+    dataset = MasksDataset(data_folder=constants.TEST_IMG_PATH, split='test')
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=20, shuffle=False)
---
+++ nvidia/train.py
@@ -0,0 +1,173 @@
+import time
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+from model import SSD300, MultiBoxLoss
+from dataset import MasksDataset
+from utils import *
+from eval import evaluate
+import constants
+
+# Label map
+masks_labels = ('proper', 'not_porper')
+label_map = {k: v + 1 for v, k in enumerate(masks_labels)}
+label_map['background'] = 0
+rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
+
+distinct_colors = ['#e6194b', '#3cb44b', '#FFFFFF']
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+
+# Model parameters
+n_classes = len(label_map)  # number of different types of objects
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Learning parameters
+batch_size = 20  # batch size
+workers = 4  # number of workers for loading data in the DataLoader
+print_freq = 200  # print training status every __ batches
+min_score = 0.01
+topk = 200
+lr = 1e-3  # learning rate TODO
+# momentum = 0.9  # momentum TODO
+weight_decay = 5e-4  # weight decay
+# clip if gradients are exploding, which may happen at larger batch sizes (sometimes at 32) -
+# you will recognize it by a sorting error in the MuliBox loss calculation
+grad_clip = None
+
+cudnn.benchmark = True
+
+checkpoint = ''  # '/home/student/checkpoint_ssd300_epoch=7.pth.tar'
+if checkpoint:
+    start_epoch = int(checkpoint.split('=')[-1].split('.')[0])
+else:
+    start_epoch = 0
+
+
+def main():
+    """
+    Training.
+    """
+    global label_map, epoch, decay_lr_at, checkpoint
+
+    # Initialize model
+    model = SSD300(n_classes=n_classes)
+    if checkpoint:
+        checkpoint = torch.load(checkpoint)
+        model.load_state_dict(checkpoint['state_dict'])
+    print(f"min_score = {min_score}")
+    print(f"top_k = {topk}")
+    # Initialize the optimizer, with twice the default learning rate for biases, as in the original Caffe repo
+    biases = list()
+    not_biases = list()
+    for param_name, param in model.named_parameters():
+        if param.requires_grad:
+            if param_name.endswith('.bias'):
+                biases.append(param)
+            else:
+                not_biases.append(param)
+    optimizer = torch.optim.Adam(params=[{'params': biases, 'lr': 2 * lr}, {'params': not_biases}],
+                                 lr=lr, weight_decay=weight_decay)  # TODO SGD with momentum=momentum \ Adam
+
+    # Move to default device
+    model = model.to(device)
+    criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy, alpha=1.).to(device)  # TODO original alpha is 1.
+
+    # Custom dataloaders
+    train_dataset = MasksDataset(data_folder=constants.TRAIN_IMG_PATH, split='train')
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                                               num_workers=workers, pin_memory=True)
+    test_dataset = MasksDataset(data_folder=constants.TEST_IMG_PATH, split='test')
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                                              num_workers=workers, pin_memory=True)
+
+    # Calculate total number of epochs to train and the epochs to decay learning rate at
+    # (i.e. convert iterations to epochs)
+    # To convert iterations to epochs, divide iterations by the number of iterations per epoch
+    # The paper trains for 120,000 iterations with a batch size of 32, decays after 80,000 and 100,000 iterations
+    epochs = 100  # TODO change
+
+    # Epochs
+    for epoch in range(start_epoch, epochs):
+        # One epoch's training
+        train(train_loader=train_loader,
+              model=model,
+              criterion=criterion,
+              optimizer=optimizer,
+              epoch=epoch)
+
+        # Save checkpoint
+        save_checkpoint(epoch, model)
+
+        # Evaluate test set
+        # TODO change to test_loader, Remove if
+        if not epoch % 40 and epoch != 0:
+            evaluate(test_loader, model, min_score=min_score, topk=topk, verbose=True)
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    """
+    One epoch's training.
+
+    :param train_loader: DataLoader for training data
+    :param model: model
+    :param criterion: MultiBox loss
+    :param optimizer: optimizer
+    :param epoch: epoch number
+    """
+    model.train()  # training mode enables dropout
+
+    batch_time = AverageMeter()  # forward prop. + back prop. time
+    data_time = AverageMeter()  # data loading time
+    losses = AverageMeter()  # loss
+
+    start = time.time()
+
+    # Batches
+    for i, (images, boxes, labels) in enumerate(train_loader):
+        data_time.update(time.time() - start)
+        # Move to default device
+        images = images.to(device)  # (batch_size (N), 3, 300, 300)
+        boxes = [b.to(device) for b in boxes]
+        labels = [l.to(device) for l in labels]
+
+        # Forward prop.
+        predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes=3)
+        # TODO YOTAM, GAL: Shoval asked me why doesn't the predicted go through model.detect_objects() as well
+        #  this is kinda weird that the train is different from the eval phase.
+        #  GAL maybe we should try first run the model without detect_objects() to improve all offsets predictions.
+
+        # Loss
+        loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar
+
+        # Backward prop.
+        optimizer.zero_grad()
+        loss.backward()
+
+        # Clip gradients, if necessary
+        if grad_clip is not None:
+            clip_gradient(optimizer, grad_clip)
+
+        # Update model
+        optimizer.step()
+
+        losses.update(loss.item(), images.size(0))
+        batch_time.update(time.time() - start)
+
+        start = time.time()
+
+        # Print status
+        if (i % print_freq == 0 or i == len(train_loader) - 1) and i != 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
+                                                                  batch_time=batch_time,
+                                                                  data_time=data_time, loss=losses))
+    del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
+
+
+# TODO
+#  1. Add early stopping based on test metrics / loss
+
+if __name__ == '__main__':
+    main()
