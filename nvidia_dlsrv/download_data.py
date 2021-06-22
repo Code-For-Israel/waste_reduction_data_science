ed1484423135d45074c7d0243ebdf8b25bba81da
yotammarton <yonam92@gmail.com>
2021-06-22 18:35:23 +0300

    DL Server run

--- nvidia_dlsrv/download_data.py
+++ nvidia_dlsrv/download_data.py
@@ -2,13 +2,13 @@ import gdown
 import tarfile
 
 url = 'https://drive.google.com/uc?id=1GsC6vtBm47kNUPrCU-8_hXU1Pi0BraE9'
-output = '/home/yotam/facemask_obj_detect/train.tar'
+output = r'/home/yotam/facemask_obj_detect/train.tar'
 gdown.download(url, output, quiet=False)
 tf = tarfile.open(output)
-tf.extractall('/home/yotam/facemask_obj_detect/')
+tf.extractall(r'/home/yotam/facemask_obj_detect/')
 
 url = 'https://drive.google.com/uc?id=1JVCAqZOhKCs3_5KrlZakB-ZJZTcyZSS3'
-output = '/home/yotam/facemask_obj_detect/test.tar'
+output = r'/home/yotam/facemask_obj_detect/test.tar'
 gdown.download(url, output, quiet=False)
 tf = tarfile.open(output)
-tf.extractall('/home/yotam/facemask_obj_detect/')
+tf.extractall(r'/home/yotam/facemask_obj_detect/')
