963e105cc88884e5889f8675e5ceb8dddc33abc9
yotammarton <yonam92@gmail.com>
2021-06-26 18:45:59 +0300

    EXP2 - Fster RCNN - READY >>>>>>>>>

--- fastrcnn/constants.py
+++ fastrcnn/constants.py
@@ -1,5 +1,5 @@
-TRAIN_IMG_PATH = '/home/student/train'
-TEST_IMG_PATH = '/home/student/test'
-#
-# TRAIN_IMG_PATH = '/mnt/ml-srv1/home/yotamm/facemask_obj_detect/train'
-# TEST_IMG_PATH = '/mnt/ml-srv1/home/yotamm/facemask_obj_detect/test'
+# TRAIN_IMG_PATH = '/home/student/train'
+# TEST_IMG_PATH = '/home/student/test'
+
+TRAIN_IMG_PATH = '/mnt/ml-srv1/home/yotamm/facemask_obj_detect/train'
+TEST_IMG_PATH = '/mnt/ml-srv1/home/yotamm/facemask_obj_detect/test'
