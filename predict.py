6e75564895b965af98011399f9f986e4d8f46ab9
yotammarton <yonam92@gmail.com>
2021-06-27 20:11:42 +0300

    Arrange code - Faster R-CNN

--- predict.py
+++ predict.py
@@ -23,20 +23,18 @@ std = [0.2642, 0.2608, 0.2561]
 
 print('Downloading model weights ...')
 module_path = os.path.dirname(os.path.realpath(__file__))
-gdrive_file_id = '1S8kCRrSA__mCI4Z0SPVuWNRty-iVlixS'  # TODO change to the best model
-
-url = f'https://drive.google.com/uc?id={gdrive_file_id}'
+# gdrive_file_id = ''  # TODO change to the best model
+#
+# url = f'https://drive.google.com/uc?id={gdrive_file_id}'
 weights_path = os.path.join(module_path, 'faster_rcnn.pth.tar')
-gdown.download(url, weights_path, quiet=False)
+# gdown.download(url, weights_path, quiet=False)
 
 print('Loading model ...')
-model = get_fasterrcnn_resnet50_fpn()
-checkpoint = torch.load(weights_path)
-model.load_state_dict(checkpoint['state_dict'])
+model = get_fasterrcnn_resnet50_fpn(weights_path=weights_path)
 
 print('Loading data ...')
 dataset = MasksDataset(data_folder=args.input_folder, split='test')
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=5, shuffle=False,
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=20, shuffle=False,
                                          num_workers=1, pin_memory=False, collate_fn=collate_fn)
 
 # Evaluate model on given data
