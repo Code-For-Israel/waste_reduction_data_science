691af4638bad4ea01f777142c202efac3647ea9d
yotammarton <yonam92@gmail.com>
2022-08-09 16:03:59 +0300

    - Try overfitting one batch - With `resnet_fpn_backbone` funciton from backbone_utils

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ def main():
                                                           shuffle=False, num_workers=workers, pin_memory=True,
                                                           collate_fn=collate_fn)
 
-    epochs = 100  # TODO More epochs
+    epochs = 1000  # TODO More epochs
     metrics = dict(train_loss=[], train_APs=[], train_mAP=[],
                    test_APs=[], test_mAP=[])
     # Epochs
